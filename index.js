e1129813690a77f658dafcf2adca844844414a0a
ozkansoyturk <soyturkozkan@gmail.com>
2023-02-25 23:45:40 +0100

    start

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World !");
+const students = [
+  {
+    name: "John",
+    notes: [1, 20, 18, 19, 12],
+  },
+  {
+    name: "Jane",
+    notes: [17, 18, 20, 13, 15],
+  },
+  {
+    name: "Sophie",
+    notes: [17, 12, 14, 15, 13],
+  },
+  {
+    name: "Marc",
+    notes: [2, 3, 5, 8, 9],
+  },
+  {
+    name: "Manon",
+    notes: [18, 17, 18, 19, 12],
+  },
+];
